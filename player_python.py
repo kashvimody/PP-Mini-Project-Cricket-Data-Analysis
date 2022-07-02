e2d639f32f54fa11d36af116e31b5b51f5cd6cdb
zeusopera9 <101108375+zeusopera9@users.noreply.github.com>
2022-07-02 10:53:07 +0530

    Update player_python.py

--- player_python.py
+++ player_python.py
@@ -1,1 +1,10 @@
+#importing essential libraries and packages
+import pandas as pd
+import numpy as np
+impo datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# This code has been written for a specific player but we shall be using this same code for all our players
+# The idea is to get the user's selection from the drop down then read the excel file for that specific player
+# Thereby provide the analysis for that player appropriately  
