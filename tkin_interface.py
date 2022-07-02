4bed0b8c5fc708e2cd80156cdc9b1c6e30182266
DarthWeeder <98643982+DarthWeeder@users.noreply.github.com>
2022-07-02 10:46:06 +0530

    Create tkin_interface.py

---
+++ tkin_interface.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import ttk
+
+
+root = Tk()
+root.title("My dropdown menu")
+root.iconbitmap(r"C:\Users\zaida\Downloads\Cover .PNG")
+root.geometry("400x400")
+
+# Drop down boxes
