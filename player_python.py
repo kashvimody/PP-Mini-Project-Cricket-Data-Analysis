843e332f9f52d28e48eebf181ff08e8fb8a45ac2
DarthWeeder <98643982+DarthWeeder@users.noreply.github.com>
2022-07-02 10:58:32 +0530

    Update player_python.py

--- player_python.py
+++ player_python.py
@@ -8,3 +8,24 @@ import seaborn as sns
 # This code has been written for a specific player but we shall be using this same code for all our players
 # The idea is to get the user's selection from the drop down then read the excel file for that specific player
 # Thereby provide the analysis for that player appropriately  
+
+str1 = "Rohit_Sharma_ODI_record"
+
+#reading the dataset
+df = pd.read_excel(str1+".xlsx")
+
+# removing the first 2 characters in the opposition string
+df['opposition'] = df['opposition'].apply(lambda x: x[2:])
+
+# creating a feature for match year
+df['year'] = df['date'].dt.year.astype(int)
+
+# creating a feature for being not out
+df['score'] = df['score'].apply(str)
+df['not_out'] = np.where(df['score'].str.endswith('*'), 1, 0)
+
+# dropping the odi_number feature because it adds no value to the analysis
+df.drop(columns='odi_number', inplace=True)
+
+# dropping those innings where Dhoni did not bat and storing in a new DataFrame
+df_new = df.loc[((df['score'] != 'DNB') & (df['score'] != 'TDNB')), 'runs_scored':]
