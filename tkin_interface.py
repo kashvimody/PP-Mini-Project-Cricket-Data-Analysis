317ebbe2c04c24b5db523dad8c96723baeacd3c6
kashvimody <96942679+kashvimody@users.noreply.github.com>
2022-07-03 13:40:44 +0530

    Update tkin_interface.py

--- tkin_interface.py
+++ tkin_interface.py
@@ -29,3 +29,34 @@ root.maxsize(1200, 988)
 myLabel = Label(text = "This is a Label")
 myLabel.pack()
 """
+
+
+p_name = [
+    "MS Dhoni",
+    "Rohit Shama",
+    "Virat Kohli",
+    "Shikhar Dhawan",
+    "Rahul Dravid",
+    "KL Rahul",
+    "Sachin Tendulkar",
+    "Yuvraj Singh",
+    "Virendar Sehwag",
+    "Gautam Gambhir",
+    "Sourav Ganguly",
+    "Ambati Rayudu"
+]
+
+xlsname = [
+    "MS_Dhoni_ODI_record",
+    "Rohit_Sharma_ODI_record",
+    "Virat_Kohli_ODI_record",
+    "Shikhar_Dhawan_ODI_record",
+    "Rahul_Dravid_ODI_record",
+    "KL_Rahul_ODI_record",
+    "Sachin_Tendulkar_ODI_record",
+    "Yuvraj_Singh_ODI_record",
+    "Virendar_Sehwag_ODI_record",
+    "Gautam_Gambhir_ODI_record",
+    "Sourav_Ganguly_ODI_record",
+    "Ambati_Rayudu_ODI_record"    
+]
